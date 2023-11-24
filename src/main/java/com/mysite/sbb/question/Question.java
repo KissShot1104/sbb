484aa2cbbb7eaf921c583130fe478f0077f8d855
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-11-25 02:49:06 +0900

    2-14 템플릿 상속 스트랩

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mysite.sbb.answer.Answer;
@@ -35,5 +36,5 @@ public class Question {
     private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    private List<Answer> answerList = new ArrayList<>();
 }
