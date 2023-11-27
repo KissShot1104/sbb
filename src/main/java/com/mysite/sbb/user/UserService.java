c7fa158ecda69315a6b77152d591ef34c1bc7061
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-11-28 05:29:07 +0900

    3-06 회원가입

---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    //BCryptPasswordEncoder는 @Bean으로 따로 만들어서 하자 그게 유지보수성면에서 더 좋다.
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+
+}
